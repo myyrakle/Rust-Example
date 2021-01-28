cdc3b08656cdac0c952d31087495a647849937c8
myyrakle <sssang97@naver.com>
2021-01-28 23:15:42 +0900

    Create impl_fn.rs

---
+++ src/impl_fn.rs
@@ -0,0 +1,32 @@
+#![feature(unboxed_closures, fn_traits)]
+
+#[derive(Clone, Copy)]
+struct Boom{}
+
+impl Fn<()> for Boom {
+    extern "rust-call" fn call(&self, _args: ()) ->()
+    {
+        println!("Boom!");
+    }
+}
+
+impl FnMut<()> for Boom {
+    extern "rust-call" fn call_mut(&mut self, _args: ()) ->()
+    {
+        println!("Boom! mut");
+    }
+}
+
+impl FnOnce<()> for Boom {
+    type Output = ();
+    extern "rust-call" fn call_once(self, _args: ()) ->()
+    {
+        println!("Boom! once");
+    }
+}
+
+fn main()
+{
+    let boom = Boom{};
+    boom();
+}
