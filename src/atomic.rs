7191f7d6a4a875f12b938dde51b48763427fbb9c
myyrakle <sssang97@naver.com>
2021-03-12 08:45:52 +0900

    Update atomic.rs

--- src/atomic.rs
+++ src/atomic.rs
@@ -4,19 +4,17 @@ use std::thread;
 
 fn main()
 {
-    let atomic = Arc::new(AtomicI32::new(1));
+    let atomic = Arc::new(AtomicI32::new(0));
     let atomic2 = Arc::clone(&atomic);
     
     let handle = thread::spawn(move ||{
-        for _ in 1..100000 {
-            let current = atomic2.load(Ordering::Acquire);
-            atomic2.store(current+1, Ordering::Release);
+        for _ in 0..100000 {
+            atomic2.fetch_add(1, Ordering::Relaxed);
         }
     });
     
-    for _ in 1..100000 {
-        let current = atomic.load(Ordering::Acquire);
-        atomic.store(current+1, Ordering::Release);
+    for _ in 0..100000 {
+        atomic.fetch_add(1, Ordering::Relaxed);
     }
     
     handle.join().unwrap();
